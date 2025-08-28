fb3255f9ad4447620a430d3eed4ca1c7a87c9a86
SebMZI <mrgy.sebastien@gmail.com>
2025-08-28 21:59:37 +0200

    feat: add dockerfile and dockerignore

--- app/_components/ChatMessage.tsx
+++ app/_components/ChatMessage.tsx
@@ -1,11 +1,26 @@
 const ChatMessage = () => {
+  const acc = false;
   return (
-    <div className="">
-      <p className="text-[10px] md:text-xs text-[#00000033]">Assistant</p>
-      <div className="w-fit px-2.5 py-3 bg-[#0000001A] border border-[#0000001A] rounded-xl shadow-xl">
-        <p className="text-xs md:text-sm text-[#000000CC]">
-          Hello, how can I help you?
+    <div className="w-full flex justify-end">
+      <div>
+        <p
+          className={`text-[10px] md:text-xs text-[#00000033] ${
+            acc ? "text-left" : "text-right"
+          }`}
+        >
+          Assistant
         </p>
+        <div
+          className={`w-fit px-2.5 py-3  border ${
+            acc
+              ? "border-[#0000001A] bg-[#0000001A]"
+              : "border-[#FFE1B21A] bg-[#FFE1B21A]"
+          } rounded-xl shadow-xl`}
+        >
+          <p className="text-xs md:text-sm text-[#000000CC]">
+            Hello, how can I help you?
+          </p>
+        </div>
       </div>
     </div>
   );
